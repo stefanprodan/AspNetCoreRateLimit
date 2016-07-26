671593589e25cc952d8fd4494c8107c056e8fc4c
Stefan Prodan <stefan.prodan@gmail.com>
2016-07-26 15:04:32 +0300

    cleanup

---
+++ test/KestrelRateLimit.Demo/Controllers/IpRateLimitController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+
+namespace KestrelRateLimit.Demo.Controllers
+{
+    [Route("api/[controller]")]
+    public class IpRateLimitController : Controller
+    {
+        private readonly IpRateLimitOptions _options;
+        private readonly IIpPolicyStore _ipPolicyStore;
+
+        public IpRateLimitController(IOptions<IpRateLimitOptions> optionsAccessor, IIpPolicyStore ipPolicyStore)
+        {
+            _options = optionsAccessor.Value;
+            _ipPolicyStore = ipPolicyStore;
+        }
+
+        [HttpGet]
+        public IpRateLimitPolicies Get()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+    }
+}
