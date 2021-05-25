39ac9cf18c5475a12c586ff0c57e6b4124263b20
Nick Cromwell <nick.cromwell@rangerlabs.io>
2021-05-25 11:59:28 -0400

    Add AspNetCoreRateLimit.Redis project

---
+++ src/AspNetCoreRateLimit.Redis/StartupExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreRateLimit.Redis
+{
+    public static class StartupExtensions
+    {
+        public static IServiceCollection AddStackExchangeRedisRateLimiting(this IServiceCollection services)
+        {
+            services.AddDistributedRateLimiting<StackExchangeRedisProcessingStrategy>();
+            return services;
+        }
+    }
+}
