cbd054cebc979dd28aea49047faeb9d90ba2d4dc
Cristi Pufu <cristian.pufu@uipath.com>
2022-12-03 19:41:44 +0200

    Fix ps build script

--- Build.ps1
+++ Build.ps1
@@ -30,6 +30,6 @@ exec { & dotnet clean -c Release }
 
 exec { & dotnet build -c Release }
 
-exec { & dotnet test -c Release -r $artifacts --no-build -l trx --verbosity=normal }
+exec { & dotnet test -c Release }
 
 exec { & dotnet pack .\src\AspNetCoreRateLimit\AspNetCoreRateLimit.csproj -c Release -o $artifacts --no-build }
